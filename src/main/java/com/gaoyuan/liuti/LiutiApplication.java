432eeef1f66c9fe57408bea926b3b3146c7139cc
wanglimeng <1592138069@qq.com>
2018-06-04 18:52:22 +0800

    Initial commit

---
+++ src/main/java/com/gaoyuan/liuti/LiutiApplication.java
@@ -0,0 +1,12 @@
+package com.gaoyuan.liuti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiutiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiutiApplication.class, args);
+	}
+}
